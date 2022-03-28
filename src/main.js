f288b7788bd5a58d044f656d351604a87eb1e6ae
xiaolin <462441355@qq.com>
2022-03-28 21:24:22 +0800

    * 引入路由

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import { createWebHashHistory } from 'vue-router'
+
+const history = createWebHashHistory()
 
 createApp(App).mount('#app')
