78bf167e169b27373f6d08700cd57760c52ab941
xiaolin <462441355@qq.com>
2022-04-05 21:19:50 +0800

    * 使用scss

--- src/lib/swish/index.vue
+++ src/lib/swish/index.vue
@@ -1,6 +1,32 @@
 <template>
-    <div>
-        swish
-    </div>
+    <button><span></span></button>
 </template>
 
+<script lang="ts">
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+button {
+    height: 22px;
+    width: 44px;
+    border: none;
+    background: blue;
+    border-radius: 11px;
+    position: relative;
+}
+
+span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: 18px;
+    width: 18px;
+    background: white;
+    border-radius: 11px;
+}
+
+</style>
