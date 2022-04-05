a233958585286e2efac3cc3959bff557072aa5c2
xiaolin <462441355@qq.com>
2022-04-05 21:35:12 +0800

    * swish 组件

--- src/lib/swish/index.vue
+++ src/lib/swish/index.vue
@@ -1,32 +1,49 @@
 <template>
-    <button><span></span></button>
+  <button :class="{checked: x}" @click="toggle"><span></span></button>
 </template>
 
 <script lang="ts">
-export default {
+import { ref } from "vue";
 
-}
+export default {
+  setup() {
+    const x = ref(false);
+    const toggle = () => {
+        x.value = !x.value
+    }
+    return {
+      x,
+      toggle
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+$h: 22px;
+$h2: $h - 4px;
 button {
-    height: 22px;
-    width: 44px;
-    border: none;
-    background: blue;
-    border-radius: 11px;
-    position: relative;
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: grey;
+  border-radius: $h/2;
+  position: relative;
 }
 
 span {
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    height: 18px;
-    width: 18px;
-    background: white;
-    border-radius: 11px;
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: 18px;
+  width: 18px;
+  background: white;
+  border-radius: 11px;
 }
-
+button.checked {
+    background: black;
+}
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
 </style>
