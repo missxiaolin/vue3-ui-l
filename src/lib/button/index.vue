0481f066491a95881d5c311ac89f3eb472493e52
xiaolin <462441355@qq.com>
2022-04-10 15:48:44 +0800

    * 完善button

--- src/lib/button/index.vue
+++ src/lib/button/index.vue
@@ -7,24 +7,36 @@
 
 <script lang="ts">
 import { computed } from "vue";
-
 export default {
   props: {
     theme: {
       type: String,
       default: "button",
     },
-    
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
     loading: {
       type: Boolean,
       default: false,
     },
   },
   setup(props) {
-    const { theme } = props;
+    const { theme, size, level } = props;
     const classes = computed(() => {
       return {
         [`crisps-button-theme-${theme}`]: theme,
+        [`crisps-button-size-${size}`]: size,
+        [`crisps-button-level-${level}`]: level,
       };
     });
     return {
@@ -34,151 +46,180 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-$h: 32px
-$border-color: #d9d9d9
-$color: #333
-$blue: #40a9ff
-$radius: 4px
-$red: #ff4848
-$grey: #a3a3a3
+<style lang="scss" scoped>
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+$red: #ff4848;
+$grey: #a3a3a3;
 
-.crisps-button
-    box-sizing: border-box
-    height: $h
-    padding: 0 12px
-    cursor: pointer
-    display: inline-flex
-    justify-content: center
-    align-items: center
-    white-space: nowrap
-    background: white
-    color: $color
-    border: 1px solid $border-color
-    border-radius: $radius
-    box-shadow: 0 1px 0 fade-out(black, 0.95)
-    transition: 500ms
+.crisps-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: 500ms;
 
-    &+&
-        margin-left: 8px
+  & + & {
+    margin-left: 8px;
+  }
+
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+
+  &:focus {
+    outline: none;
+  }
+
+  &::-moz-focus-inner {
+    border: 0;
+  }
+
+  &.crisps-button-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
 
     &:hover,
-    &:focus
-        color: $blue
-        border-color: $blue
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
 
-    &:focus
-        outline: none
+  &.crisps-button-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
 
-    &::-moz-focus-inner
-        border: 0
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
 
-        &&-theme-link
-            border-color: transparent
-            box-shadow: none
-            color: $blue
+  &.crisps-button-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
 
-        &:hover,
-        &:focus
-            color: lighten($blue, 10%)
+  &.crisps-button-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
 
-        &&-theme-text
-            border-color: transparent
-            box-shadow: none
-            color: inherit
+  &.crisps-button-theme-button {
+    &.crisps-button-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
 
-        &:hover,
-        &:focus
-            background: darken(white, 5%)
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
 
-        &&-size-big
-            font-size: 24px
-            height: 48px
-            padding: 0 16px
+    &.crisps-button-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
 
-        &&-size-small
-            font-size: 12px
-            height: 20px
-            padding: 0 4px
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
 
-        &&-theme-button
+  &.crisps-button-theme-link {
+    &.crisps-button-level-danger {
+      color: $red;
 
-            &&-level-main
-                background: $blue
-                color: white
-                border-color: $blue
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
 
-        &:hover,
-        &:focus
-            background: darken($blue, 10%)
-            border-color: darken($blue, 10%)
+  &.crisps-button-theme-text {
+    &.crisps-button-level-main {
+      color: $blue;
 
-            &&-level-danger
-                background: $red
-                border-color: $red
-                color: white
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
 
-        &:hover,
-        &:focus
-            background: darken($red, 10%)
-            border-color: darken($red, 10%)
+    &.crisps-button-level-danger {
+      color: $red;
 
-        &&-theme-link
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
 
-            &&-level-danger
-                color: $red
+  &.crisps-button-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      background: #dbdbdb;
 
-        &:hover,
-        &:focus
-            color: darken($red, 10%)
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
 
-        &&-theme-text
+  &.crisps-button-theme-link,
+  &.crisps-button-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
 
-            &&-level-main
-                color: $blue
+  > .crisps-button-loadingIndicator {
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: crisps-button-spin 1s infinite linear;
+  }
+}
 
-        &:hover,
-        &:focus
-            color: darken($blue, 10%)
+@keyframes crisps-button-spin {
+  0% {
+    transform: rotate(0deg);
+  }
 
-            &&-level-danger
-                color: $red
-
-        &:hover,
-        &:focus
-            color: darken($red, 10%)
-
-        &&-theme-button
-        &[disabled]
-            cursor: not-allowed
-            color: $grey
-            background: #dbdbdb
-
-            &:hover
-                border-color: $grey
-
-    &.crisps-button-theme-link,
-
-        &&-theme-text
-        &[disabled]
-            cursor: not-allowed
-            color: $grey
-
-        >&-loadingIndicator
-            width: 14px
-            height: 14px
-            display: inline-block
-            margin-right: 4px
-            border-radius: 8px
-            border-color: $blue $blue $blue transparent
-            border-style: solid
-            border-width: 2px
-            animation: crisps-button-spin 1s infinite linear
-
-@keyframes crisps-button-spin
-    0%
-        transform: rotate(0deg)
-
-    100%
-        transform: rotate(360deg)
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
