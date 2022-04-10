e1c17883097a883832ad6b275c4dc842e7609c83
xiaolin <462441355@qq.com>
2022-04-10 21:54:07 +0800

    * slot

--- src/lib/tabs/index.vue
+++ src/lib/tabs/index.vue
@@ -1,5 +1,20 @@
 <template>
     <div>
-        tabs
+        <component :is="defaults[0]" />
+        <component :is="defaults[1]" />
+        <component :is="defaults[2]" />
     </div>
 </template>
+
+<script lang="ts">
+
+export default {
+    setup(props, context) {
+        const defaults = context.slots.default()
+        console.log(defaults)
+        return {
+            defaults
+        }
+    }
+}
+</script>
