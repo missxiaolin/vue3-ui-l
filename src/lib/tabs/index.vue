a7935b747bf85f0d45cb403003105472521f1482
xiaolin <462441355@qq.com>
2022-04-11 15:43:16 +0800

    * 渲染tabs

--- src/lib/tabs/index.vue
+++ src/lib/tabs/index.vue
@@ -1,20 +1,30 @@
 <template>
-    <div>
-        <component :is="defaults[0]" />
-        <component :is="defaults[1]" />
-        <component :is="defaults[2]" />
+  <div>
+    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
+    <div class="crisps-tabs-content">
+      <component v-for="(c, index) in defaults" :is="c" :key="index" />
     </div>
+  </div>
 </template>
 
 <script lang="ts">
+import Tab from "../tab/index.vue";
 
 export default {
-    setup(props, context) {
-        const defaults = context.slots.default()
-        console.log(defaults)
-        return {
-            defaults
-        }
-    }
-}
+  setup(props, context) {
+    const defaults = context.slots.default();
+    defaults.forEach((tab) => {
+      if (tab.type !== Tab) {
+        throw new Error("Tabs 子标签必须为 Tab");
+      }
+    });
+    const titles = defaults.map((tag: any) => {
+      return tag.props.title;
+    });
+    return {
+      titles,
+      defaults,
+    };
+  },
+};
 </script>
