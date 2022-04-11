5b4db3ae1245d18bc936161daff273db4a07597c
xiaolin <462441355@qq.com>
2022-04-11 21:38:41 +0800

    * tab

--- src/lib/tabs/index.vue
+++ src/lib/tabs/index.vue
@@ -3,6 +3,7 @@
     <div class="crisps-tabs-nav" ref="container">
       <div
         class="crisps-tabs-nav-item"
+        @click="select(t)"
         :class="{
           selected: t.title === value,
           'crisps-tabs-nav-disabled': t.disabled,
@@ -20,14 +21,18 @@
       <div class="crisps-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="crisps-tabs-content">
-      <component v-for="(c, index) in defaults" :is="c" :key="index" />
+      <component
+        class="crisps-tabs-content-item"
+        :is="current"
+        :key="current.props.title"
+      />
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { computed, ref, onMounted, watchEffect } from "vue";
 import Tab from "../tab/index.vue";
-
 export default {
   props: {
     value: {
@@ -35,25 +40,69 @@ export default {
     },
   },
   setup(props, context) {
+    const selectedItem = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    onMounted(() => {
+      watchEffect(
+        () => {
+          const { width } = selectedItem.value.getBoundingClientRect();
+          indicator.value.style.width = width + "px";
+          const { left: containerLeft } =
+            container.value.getBoundingClientRect();
+          const { left: resultLeft } =
+            selectedItem.value.getBoundingClientRect();
+          const left = resultLeft - containerLeft;
+          indicator.value.style.left = left + "px";
+        },
+        {
+          flush: "post",
+        }
+      );
+    });
+
     const defaults = context.slots.default();
     defaults.forEach((tab) => {
       if (tab.type !== Tab) {
         throw new Error("Tabs 子标签必须为 Tab");
       }
     });
-    const titles = defaults.map((tag: any) => {
-      return tag.props.title;
+    const titles = defaults.map((tab) => {
+      if (tab.props.disabled || tab.props.disabled === "") {
+        return {
+          title: tab.props.title,
+          disabled: true,
+        };
+      } else {
+        return {
+          title: tab.props.title,
+          disabled: false,
+        };
+      }
     });
+    const select = (t: { title: string; disabled: boolean }) => {
+      if (!t.disabled) {
+        context.emit("update:value", t.title);
+      }
+    };
+    const current = computed(() => {
+      return defaults.find((tab) => tab.props.title === props.value);
+    });
+
     return {
-      titles,
       defaults,
+      titles,
+      current,
+      select,
+      selectedItem,
+      indicator,
+      container,
     };
   },
 };
 </script>
 
-
-<style lang="scss" scoped>
+<style lang="scss">
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
--- src/view/tabs/index.vue
+++ src/view/tabs/index.vue
@@ -1,5 +1,5 @@
 <template>
-    <Tabs>
+    <Tabs v-model:value="name">
         <Tab title="导航1">内容1</Tab>
         <Tab title="导航2">内容2</Tab>
         <Tab title="导航3">内容3</Tab>
@@ -10,11 +10,20 @@
 <script lang="ts">
 import Tabs from '../../lib/tabs/index.vue'
 import Tab from '../../lib/tab/index.vue'
+import {
+    ref
+} from 'vue';
 
 export default {
     components: {
-        Tabs,
-        Tab
+        Tab,
+        Tabs
+    },
+    setup() {
+        const name = ref("导航1");
+        return {
+            name
+        }
     }
 }
 </script>
