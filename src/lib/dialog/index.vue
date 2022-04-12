f9c1a1e29a4f60ee1d54e581b147ae28294b7c8d
xiaolin <462441355@qq.com>
2022-04-12 21:16:24 +0800

    * 初始化入参

--- src/lib/dialog/index.vue
+++ src/lib/dialog/index.vue
@@ -3,3 +3,24 @@
         Dialog
     </div>
 </template>
+
+<script>
+export default {
+    props: {
+        visible: {
+            type: Boolean,
+            default: false,
+        },
+        maskClosable: {
+            type: Boolean,
+            default: true,
+        },
+        ok: {
+            type: Function,
+        },
+        cancel: {
+            type: Function,
+        },
+    },
+}
+</script>
--- src/view/dialog/index.vue
+++ src/view/dialog/index.vue
@@ -1,12 +1,43 @@
 <template>
-    <Dialog />
+  <div>
+    <Button @click="toggle">toggle</Button>
+    <Dialog :ok="clickOk" :cancel="clickCancel" :maskClosable="true" v-model:visible="isVisible">
+        <template v-slot:title>
+            <strong>这是一个提示</strong>
+        </template>
+        <template v-slot:content>
+            <div>薯片！</div>
+            <div>好吃！</div>
+        </template>
+    </Dialog>
+  </div>
 </template>
 
 <script>
-import Dialog from '../../lib/dialog/index.vue'
+import Button from "../../lib/button/index.vue";
+import Dialog from "../../lib/dialog/index.vue";
+import { ref } from "vue";
+
 export default {
-    components: {
-        Dialog
-    }
-}
+  components: {
+    Dialog,
+    Button,
+  },
+  setup() {
+    const isVisible = ref(false);
+    const toggle = () => {
+      isVisible.value = !isVisible.value;
+    };
+    const clickOk = () => {
+      return true;
+    };
+    const clickCancel = () => {};
+    return {
+      isVisible,
+      toggle,
+      clickOk,
+      clickCancel,
+    };
+  },
+};
 </script>
