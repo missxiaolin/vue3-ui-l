0e614e1f9f4e4af717f093b9111080abedaceed6
xiaolin <462441355@qq.com>
2022-04-15 14:34:20 +0800

    * 增加包

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 import './asset/css/index.scss'
-
+import 'github-markdown-css'
 const app = createApp(App)
 
 app.use(router)
