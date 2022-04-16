64949112d4269737e74ce173f81712b43db9d8d7
xiaolin <462441355@qq.com>
2022-04-16 22:10:26 +0800

    * 做成组件

---
+++ src/markdown/Markdown.vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="markdown-body" v-html="content"></article>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  props: {
+    path: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    let content: any = ref<string>(null);
+    import(props.path).then((res: any) => {
+        content.value = res.default
+    })
+    return {
+        content
+    }
+  },
+};
+</script>
--- src/view/intro/index.vue
+++ src/view/intro/index.vue
@@ -1,12 +1,17 @@
 <template>
     <article class="markdown-body" v-html="md">
     </article>
+    <MarkDown path="../markdown/install.md" />
 </template>
 
 <script lang="ts">
 import md from '../../markdown/install.md'
-console.log(md)
+import MarkDown from '../../markdown/Markdown.vue'
+
 export default {
+    components: {
+        MarkDown
+    },
     data() {
         return {
             md
