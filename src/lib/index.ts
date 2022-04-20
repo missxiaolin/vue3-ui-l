ef1aa19e0b63cfabfd82fb1c40a8e35d6556f8fc
xiaolin <462441355@qq.com>
2022-04-20 21:38:06 +0800

    * 创建rollup.js

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
+export {default as Button} from './button/index.vue';
+export {default as Switch} from './swish/index.vue';
+export {default as Dialog} from './dialog/index.vue';
+export {default as Tab} from './tab/index.vue';
+export {default as Tabs} from './tabs/index.vue';
 export {openDialog as openDialog} from './dialog/openDialog';
