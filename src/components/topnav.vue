775176c23c5a2d29cfe5c16b82d43dcbd0e6f539
xiaolin <462441355@qq.com>
2022-04-22 20:37:21 +0800

    * 初始化组件

---
+++ src/components/topnav.vue
@@ -0,0 +1,128 @@
+<template>
+  <div>
+    <div class="topnav" ref="topnav">
+      <router-link ref="topLogo" to="/" class="logo">
+        <svg class="icon">
+          <use xlink:href="#icon-food-crisps"></use>
+        </svg>
+      </router-link>
+      <ul class="menu">
+        <li>
+          <router-link to="/doc">文档</router-link>
+        </li>
+      </ul>
+      <svg
+        v-if="toggleMenuVisible"
+        @click="toggleMenu"
+        class="toggleAside icon"
+      >
+        <use xlink:href="#icon-menu" />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { inject, Ref, onMounted, onUnmounted, ref, watchEffect } from "vue";
+export default {
+  props: {
+    toggleMenuVisible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("asideVisible");
+    const topnav = ref<HTMLDivElement>(null);
+    const toggleMenu = () => {
+      asideVisible.value = !asideVisible.value;
+    };
+    const handleScroll = () => {};
+    onMounted(() => {
+      window.addEventListener("scroll", handleScroll);
+    });
+    onUnmounted(() => {
+      window.removeEventListener("scroll", handleScroll);
+    });
+    return {
+      toggleMenu,
+      topnav,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.topnav {
+  display: flex;
+  padding: 16px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 10;
+
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+    margin-left: 100px;
+
+    > svg {
+      width: 48px;
+      height: 48px;
+    }
+  }
+
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+    margin-right: 100px;
+
+    > li {
+      margin: 0 1em;
+      color: #b0462a;
+
+      a {
+        font-size: 20px;
+      }
+    }
+  }
+
+  > .toggleAside {
+    display: none;
+    width: 32px;
+    height: 32px;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    background: fade-out(black, 0.9);
+  }
+
+  @media (max-width: 896px) {
+    position: fixed;
+    padding: 0;
+    padding-top: 5px;
+    background-color: #edeceb;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
+    > .menu {
+      display: none;
+    }
+
+    > .logo {
+      margin: 0 auto;
+
+      > svg {
+        width: 2.2em;
+        height: 2.2em;
+      }
+    }
+
+    > .toggleAside {
+      display: inline-block;
+    }
+  }
+}
+</style>
---
+++ src/view/doc.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="layout">
+    <Topnav toggleMenuVisible class="nav" />
+    <div class="content">
+      <aside v-if="asideVisible">
+        <div>
+          <h2>文档</h2>
+          <ol>
+            <li>
+              <router-link to="/doc/introduce">介绍</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/install">安装</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/get-started">快速上手</router-link>
+            </li>
+          </ol>
+          <h2>组件列表</h2>
+          <ol>
+            <li>
+              <router-link to="/doc/switch">Switch 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/button">Button 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/dialog">Dialog 组件</router-link>
+            </li>
+            <li>
+              <router-link to="/doc/tabs">Tabs 组件</router-link>
+            </li>
+          </ol>
+        </div>
+      </aside>
+      <main @click="toggleAsideVisible">
+        <router-view />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Topnav from "../components/topnav.vue";
+import {
+  inject,
+  onMounted,
+  onUnmounted,
+  provide,
+  reactive,
+  ref,
+  Ref,
+  watchEffect,
+} from "vue";
+import { router } from "../router/index.ts";
+import { debounce } from "../utils/debounce";
+export default {
+  name: "Doc",
+  components: {
+    Topnav,
+  },
+
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("asideVisible");
+    const data = reactive({
+      listenerPageWidthFn: () => {},
+      pageWidth: document.documentElement.clientWidth,
+    });
+
+    const watchPageWidth = () => {
+      const listenerPageWidth = debounce(() => {
+        data.pageWidth = document.documentElement.clientWidth;
+      }, 300);
+
+      window.addEventListener("resize", listenerPageWidth);
+
+      return listenerPageWidth;
+    };
+    const toggleAsideVisible = () => {
+      if (data.pageWidth <= 896) {
+        asideVisible.value = false;
+      }
+    };
+    watchEffect(() => {
+      if (data.pageWidth >= 896) {
+        asideVisible.value = true;
+      }
+    });
+
+    onMounted(() => {
+      data.listenerPageWidthFn = watchPageWidth();
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("resize", data.listenerPageWidthFn);
+    });
+
+    return {
+      asideVisible,
+      toggleAsideVisible,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../asset/css/index.scss";
+$padding-spacing: 200px;
+
+.router-link-active {
+  color: #c66844;
+  background: #9fcfc4;
+}
+
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  > .nav {
+    flex-shrink: 0;
+  }
+
+  > .content {
+    flex-grow: 1;
+    padding: 0 40px 0 340px;
+    display: flex;
+
+    > aside {
+      flex-shrink: 0;
+      background: rgba($color: #000000, $alpha: 0.2);
+      min-width: 300px;
+      padding: 16px 0;
+      position: fixed;
+      top: 0;
+      left: 0;
+      padding-top: 85px;
+      height: 100%;
+      z-index: 5;
+
+      > div {
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+
+        > h2 {
+          margin-bottom: 4px;
+          padding-left: 16px;
+        }
+
+        > ol {
+          > li {
+            a {
+              display: block;
+              padding: 10px 32px;
+              color: lighten($fontColor, 14%);
+            }
+          }
+        }
+      }
+    }
+
+    > main {
+      flex-grow: 1;
+      padding: 16px;
+      background: rgba($color: #000000, $alpha: 0.1);
+      border-radius: 6px;
+      overflow: auto;
+    }
+
+    @media (max-width: 896px) {
+      padding: 44px 0 0 0;
+
+      > aside {
+        background: #f9f9f9;
+      }
+
+      > main {
+        background: $bgColor;
+      }
+    }
+
+    @media (max-width: 500px) {
+      > aside {
+        min-width: 50%;
+      }
+    }
+  }
+}
+</style>
