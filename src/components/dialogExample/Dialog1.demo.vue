a1065f4dfd8babec43d79db9b89f7910df7dc80b
xiaolin <462441355@qq.com>
2022-04-24 21:04:57 +0800

    * dialog

---
+++ src/components/dialogExample/Dialog1.demo.vue
@@ -0,0 +1,45 @@
+<demo>基本</demo>
+<template>
+<div>
+    <Button @click="toggle">toggle</Button>
+    <Dialog :ok="clickOk" :cancel="clickCancel" :maskClosable="true" v-model:visible="isVisible">
+        <template v-slot:title>
+            <strong>这是一个提示</strong>
+        </template>
+        <template v-slot:content>
+            <div>薯片！</div>
+            <div>好吃！</div>
+        </template>
+    </Dialog>
+</div>
+</template>
+
+<script lang="ts">
+import Button from '../../lib/button/index.vue'
+import Dialog from '../../lib/dialog/index.vue'
+import {
+    ref
+} from 'vue';
+export default {
+    components: {
+        Dialog,
+        Button
+    },
+    setup() {
+        const isVisible = ref(false);
+        const toggle = () => {
+            isVisible.value = !isVisible.value;
+        };
+        const clickOk = () => {
+            return true;
+        };
+        const clickCancel = () => {};
+        return {
+            isVisible,
+            toggle,
+            clickOk,
+            clickCancel
+        }
+    }
+}
+</script>
