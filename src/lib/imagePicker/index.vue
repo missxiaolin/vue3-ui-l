53f0ef02ca70255a1edce9f73c7d95f0111af88a
xiaolin <462441355@qq.com>
2022-04-28 22:11:15 +0800

    * 上传图片

--- src/lib/imagePicker/index.vue
+++ src/lib/imagePicker/index.vue
@@ -13,44 +13,43 @@
 
 
 <script lang="ts">
-import { onMounted } from 'vue'
-import ImagePicker from '../utils/image'
+import { onMounted } from "vue";
+import ImagePicker from "../utils/image";
 
 export default {
   setup() {
-
     onMounted(() => {
       new ImagePicker({
-        element: document.querySelector('.image-picker'),
+        element: document.querySelector(".image-picker"),
         upload: {
-          url: 'https://frankfang.com/image-server/upload',
-          method: 'PUT',
-          inputName: 'file'
+          url: "https://baidu.com/image-server/upload",
+          method: "PUT",
+          inputName: "file",
         },
         parseResponse: (response) => {
-          response = JSON.parse(response)
-          return `https://frankfang.com/image-server/upload/${response.key}`
+          response = JSON.parse(response);
+          return `https://baidu.com/image-server/upload/${response.key}`;
         },
-        fallbackImage: 'https://avatars0.githubusercontent.com/u/839559'
-      })
-    })
-    
-  }
-}
+        fallbackImage:
+          "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2F1113%2F052420110515%2F200524110515-2-1200.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1653657652&t=46a8f2abd3f9a62ed16f068782e004ce",
+      });
+    });
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .card {
-    background: white;
-    width: 20em;
-    height: 80vh;
-    box-shadow: 0 0 5px hsla(0,0%,0%,0.95);
-    border-radius: 2px;
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    padding-top: 3em;
-    flex-direction: column;
+  background: white;
+  width: 20em;
+  height: 80vh;
+  box-shadow: 0 0 5px hsla(0, 0%, 0%, 0.95);
+  border-radius: 2px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding-top: 3em;
+  flex-direction: column;
 }
 .image-picker {
   width: 100px;
--- src/lib/utils/image.ts
+++ src/lib/utils/image.ts
@@ -1,4 +1,23 @@
-import dom from './dom'
+import dom from "./dom";
+
+function prefetch(url) {
+  return new Promise((resolve, reject) => {
+    let img = new Image();
+    img.onload = resolve;
+    img.onerror = reject;
+    img.src = url;
+  });
+}
+
+function http(method, url, data) {
+  return new Promise((resolve, reject) => {
+    let xhr: any = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = () => resolve(xhr.responseText, xhr);
+    xhr.onerror = () => reject(xhr);
+    xhr.send(data);
+  });
+}
 
 export default class ImagePicker {
   public options: any;
@@ -36,10 +55,80 @@ export default class ImagePicker {
   }
 
   initHtml() {
-    let { element } = this.options
-    let fileInput = (this.domRefs.fileInput = dom.create('<input type="file"/>'))
-    dom.append(element, fileInput)
+    let { element } = this.options;
+    let fileInput = (this.domRefs.fileInput = dom.create(
+      '<input type="file"/>'
+    ));
+    dom.append(element, fileInput);
   }
 
-  bindEvents() {}
+  willUpload(formData) {
+    this.options.element.classList.add("uploading");
+    this.domRefs.fileInput.disabled = true;
+  }
+
+  didUpload(formData) {
+    let { element } = this.options;
+    element.classList.remove("uploading");
+    this.domRefs.fileInput.disabled = false;
+    this.domRefs.fileInput.value = "";
+    dom.dispatchEvent(element, "uploaded");
+  }
+
+  failedUpload(formData) {
+    let { element } = this.options;
+    this.domRefs.fileInput.disabled = false;
+    this.domRefs.fileInput.value = "";
+    dom.dispatchEvent(element, "uploadFailed");
+  }
+
+  willDownload(path) {
+    this.options.element.classList.add("downloading");
+  }
+
+  didDownload(path) {
+    this.domRefs.img.src = path;
+    let { element } = this.options;
+    element.classList.remove("downloading");
+    dom.dispatchEvent(element, "uploadedImageLoaded");
+  }
+
+  failedDownload(path?: any) {
+    let { element, fallbackImage } = this.options;
+    element.classList.remove("downloading");
+    if (fallbackImage) {
+      this.domRefs.img.src = fallbackImage;
+    }
+    dom.dispatchEvent(element, "uploadedImageFailed");
+  }
+
+  upload(formData) {
+    let { element, upload, parseResponse } = this.options;
+    http(upload.method, upload.url, formData).then(
+      (responseBody) => {
+        let path = parseResponse(responseBody);
+        this.didUpload(formData);
+        this.willDownload(path);
+        prefetch(path).then(
+          () => {
+            this.didDownload(path);
+          },
+          () => {
+            this.failedDownload();
+          }
+        );
+      },
+      () => this.failedUpload(formData)
+    );
+  }
+
+  bindEvents() {
+    this.domRefs.fileInput.addEventListener("change", (e) => {
+      let { upload } = this.options;
+      let formData = new FormData();
+      formData.append(upload.inputName, e.target.files[0]);
+      this.willUpload(formData);
+      this.upload(formData);
+    });
+  }
 }
